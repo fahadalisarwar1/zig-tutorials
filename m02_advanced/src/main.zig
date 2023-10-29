fb33aff17d03ed0120920e53b27141e7387aab7a
fahad <fahadalisarwar@gmail.com>
2023-10-29 19:34:56 +0100

    update

--- m02_advanced/src/main.zig
+++ m02_advanced/src/main.zig
@@ -3,28 +3,25 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn main() !void {
-    
-    //lets take a look at advanced features of zig. 
+
+    //lets take a look at advanced features of zig.
     // for loops
-    const string = [_]u8{'a', 'b', 'c', 'd', 'e', 'f'};
-    for (string, 0..)|ch, index|{
-        print("{} {}\n", .{index, ch});
+    const string = [_]u8{ 'a', 'b', 'c', 'd', 'e', 'f' };
+    for (string, 0..) |ch, index| {
+        print("{} {}\n", .{ index, ch });
     }
 
-    // all function arguments are immutable, if a copy is required it must be made explicitly. 
-    // defer statement 
-    // defer is used when you want to execute some statement when leaving a block, like memory free or close connection etc. 
+    // all function arguments are immutable, if a copy is required it must be made explicitly.
+    // defer statement
+    // defer is used when you want to execute some statement when leaving a block, like memory free or close connection etc.
 
     var x: i16 = 23;
     {
         print("x before defer statement {}\n", .{x});
         defer x = 43;
-        print("x after defer statement {}\n",.{x});
-
-
+        print("x after defer statement {}\n", .{x});
     }
     print("x after leaving block {}\n", .{x});
-
 
     // ERRORS
 
@@ -34,17 +31,10 @@ pub fn main() !void {
         FILE_NOT_FOUND,
     };
     var f_error: FileError = FileError.ACCESS_DENIED;
-    print("error is {}\n",.{f_error});
+    print("error is {}\n", .{f_error});
 
     //error set
 
-    const maybe_error: FileError!u16 =10;
+    const maybe_error: FileError!u16 = 10;
     print("maybe_error = {any}\n", .{maybe_error});
-
-
-
-
-    
-
 }
-
